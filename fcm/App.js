a056925c82bf738165ef7c14238da535d8562cee
tecknovice <tecknovice@gmail.com>
2019-11-26 11:30:45 +0700

    simplify App.js

--- fcm/App.js
+++ fcm/App.js
@@ -1,57 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   View,
   Text,
   StyleSheet
 } from 'react-native';
-import messaging from '@react-native-firebase/messaging';
 
-const App = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>FCM</Text>
-      <Text style={styles.sectionDescription}>Firebase Cloud Messaging Demo</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+      </View>
+    );
+  }
+}
